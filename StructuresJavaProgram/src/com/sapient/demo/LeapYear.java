6b8e0a061c37c9885f22d55801e4782d332f0eb6
pku164 <pku164@GIVSARDIN38974.sapient.com>
2017-12-20 17:27:53 +0530

    20/12/17 java program

--- StructuresJavaProgram/src/com/sapient/demo/LeapYear.java
+++ StructuresJavaProgram/src/com/sapient/demo/LeapYear.java
@@ -1,9 +1,28 @@
 package com.sapient.demo;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int yr;
+		System.out.println("Enter the year : ");
+		yr = sc.nextInt();
+		if(yr%4==0){
+			if(yr%100 == 0){
+				if(yr%400 == 0)
+					System.out.println("The entered year " + yr + "is a LEAP YEAR ");
+				else
+					System.out.println("The entered year " + yr + "is NOT a LEAP YEAR ");
+		    }
+			else
+				System.out.println("The entered year " + yr + "is a LEAP YEAR ");
+		}
+		else
+			System.out.println("The entered year " + yr + "is NOT aLEAP YEAR ");
+		sc.close();
 
 	}
 
